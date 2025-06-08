a687809bb3876f76b0501733d60a1f3086507008
La Min Maung <maungl@oregonstate.edu>
2025-06-08 12:48:09 -0700

    main.c

--- test/main.c
+++ test/main.c
@@ -1,16 +1,20 @@
-#include <stdio.h>
 #include "debug.h"
 
-void g(void) {
-    dump_registers();
-    dump_backtrace();
+void g(int rdi, int rsi, int rdx, int rcx, int r8, int r9)
+{
+  dump_registers();
+  dump_backtrace();
 }
 
-void f(void) {
-    g();
+int no_op(void) { return 42; }
+
+void f()
+{
+  no_op();
+  g(1, 2, 3, 4, 5, 6);
 }
 
-int main(void) {
-    f();
-    return 0;
+int main()
+{
+  f();
 }
